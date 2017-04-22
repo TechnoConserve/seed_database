5259acecfdd3554675b89b732202c873b054bcd9
Avery Uslaner <uslaner.avery@gmail.com>
2017-04-22 16:38:57 -0600

    Refactored indentation, fixed bugs in display of shipment form.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for
-import pandas as pd
+from flask import Flask, redirect, render_template, flash
 
 import forms
 import models
@@ -14,30 +13,38 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/shipment', methods=('GET', 'POST'))
+@app.route('/shipments', methods=('GET', 'POST'))
 def shipment():
-  form = forms.ShipmentForm()
-  form.origin_institute_id.choices = [
-    (inst.id, inst.name) for inst in models.Institution.query.order_by('name')]
-  form.destination_institute_id.choices = [
-    (inst.id, inst.name) for inst in models.Institution.query.order_by('name')]
-  form.accession.choices = [
-    (acc.id, acc.acc_num) for acc in models.Accession.query.order_by('acc_num')]
-  if form.validate_on_submit():
-    flash('Yay, Shipment created!', 'success')
-    shipment = models.Shipping(
-      ship_date=form.ship_date.data, 
-      tracking_num=form.tracking_num.data,
-      tracking_num_comp=form.tracking_num_comp.data, 
-      amount_gr=form.amount_gr.data, 
-      calc_by=form.calc_by.data,
-      origin_institute=form.origin_instiute.data, 
-      destination_institute=form.destination_institute.data,
-      accession=form.accession.data,
-    )
+    form = forms.ShipmentForm()
+    form.origin_institute_id.choices = [
+        (inst.id, inst.name) for inst in models.Institution.query.order_by('name')]
+    form.destination_institute_id.choices = [
+        (inst.id, inst.name) for inst in models.Institution.query.order_by('name')]
+    form.accession.choices = [
+        (acc.id, acc.acc_num) for acc in models.Accession.query.order_by('acc_num')]
+    if form.validate_on_submit():
+        flash('Yay, Shipment created!', 'success')
+        shipment = models.Shipping(
+            ship_date=form.ship_date.data,
+            tracking_num=form.tracking_num.data,
+            tracking_num_comp=form.tracking_num_comp.data,
+            amount_gr=form.amount_gr.data,
+            calc_by=form.calc_by.data,
+            origin_institute=form.origin_instiute.data,
+            destination_institute=form.destination_institute.data,
+            accession=form.accession.data,
+        )
+        return redirect('/success')
+    return render_template('shipments.html', form=form)
+
+
+@app.route('/success')
+def success():
+    return 'Success!'
+
 
 if __name__ == '__main__':
-  models.db.init_app(app)
-  models.db.create_all()
+    models.db.init_app(app)
+    models.db.create_all()
 
-  app.run()
+    app.run()
--- forms.py
+++ forms.py
@@ -1,39 +1,39 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import DateField, FloatField, SelectField, StringField
-from wtforms.validators import (DataRequired, Regexp, ValidationError, Email,
-                                Length, EqualTo)
+from wtforms.validators import (DataRequired, ValidationError)
 
 from models import Shipping
 
-COMPANIES = ['FedEx', 'UPS', 'USPS']
+COMPANIES = [('fx', 'FedEx'), ('ups', 'UPS'), ('usps', 'USPS')]
 
-def shipment_exists(form, field):
-  if Shipping.select().where(Shipping.tracking_num == field.data).exists():
-    raise ValidationError('Shipment with that tracking number already exists.')
-    
-class ShipmentForm(Form):
-  ship_date = DateField(
-    'Shipment Date',
-    validators=[DataRequired()],
-    format='%m-%d-%Y')
-  tracking_num = StringField(
-    'Tracking Number',
-    validators=[
-      DataRequired(),
-      shipment_exists
-    ])
-  tracking_num_comp = SelectField(
-    'Company',
-    validators=[DataRequired()],
-    choices=COMPANIES)
-  amount_gr = FloatField(
-    'Amount in grams',
-    validators=[DataRequired()])
-  calc_by = SelectField(
-    'Amount calculated by...',
-    validators=[DataRequired()],
-    choices=['Counting', 'Weighed'])
-  origin_institute_id = SelectField('Origin', coerce=int)
-  destination_institute_id = SelectField('Destination', coerce=int)
-  accession = SelectField('Accession', coerce=int)
-  
+
+def shipment_exists(field):
+    if Shipping.select().where(Shipping.tracking_num == field.data).exists():
+        raise ValidationError('Shipment with that tracking number already exists.')
+
+
+class ShipmentForm(FlaskForm):
+    ship_date = DateField(
+        'Shipment Date',
+        validators=[DataRequired()],
+        format='%m-%d-%Y')
+    tracking_num = StringField(
+        'Tracking Number',
+        validators=[
+            DataRequired(),
+            shipment_exists
+        ])
+    tracking_num_comp = SelectField(
+        'Company',
+        validators=[DataRequired()],
+        choices=COMPANIES)
+    amount_gr = FloatField(
+        'Amount in grams',
+        validators=[DataRequired()])
+    calc_by = SelectField(
+        'Amount calculated by...',
+        validators=[DataRequired()],
+        choices=[('ct', 'Counting'), ('wt', 'Weighed')])
+    origin_institute_id = SelectField('Origin', coerce=int)
+    destination_institute_id = SelectField('Destination', coerce=int)
+    accession = SelectField('Accession', coerce=int)
